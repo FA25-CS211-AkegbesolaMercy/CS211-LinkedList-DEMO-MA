7580fca3fe5d7ec0516a78d4b0b937996befff28
Mercy Akegbesola <akegbesolamercy@myocu.oak.edu>
2025-10-30 13:13:05 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+"""
+File Name: main.py
+Project Name: Linked List Demo
+Author: Akegbesola Mercy
+Date Created: 10/29/2025
+"""
+
+import LinkedList as ll
+
+def main(): 
+
+# Initialize empty ll
+head = None
+
+print("Linked List Demo")
+
+# element insertion at the end
+print("Inserting elements at the end of the linked list")
+head = ll.insert_at_end(head, data:25)
+head = ll.insert_at_end(head, data:58)
+head = ll.insert_at_end(head, data:75)
+
+# Element insertion  
+
+
+
+# Element insertion at the beginning
+
+# Get the lenght of the ll
+
+# Search for an element in the ll
+
+# Search for element that does not exist
+# (think about using conditional logic to handle this case)
+
+# Delete an element in the ll
+
+# Get the final lenght of the ll( Think line 19)
+
+
+
